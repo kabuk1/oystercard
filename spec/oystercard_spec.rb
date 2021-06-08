cd48bc4e2eff69c562cf29b334aa6dd450f1c118
Leslie <leslie.liszcz@gmail.com>
2021-06-08 14:37:59 +0100

    challege 6: deduct the money

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,18 +1,24 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
+
   it 'shows the card balance' do
-    expect(subject.balance).to eq(0)
+    expect(oystercard.balance).to eq(0)
   end
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
   it 'can top-up the card balance' do
-    expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+    expect{ oystercard.top_up 5 }.to change{ oystercard.balance }.by 5
   end
 
   it 'raises and error when top-up limit exceeded' do
-    expect{ subject.top_up 100 }.to raise_error 'Exceeds top-up limit of £90'
+    expect{ oystercard.top_up 100 }.to raise_error 'Exceeds top-up limit of £90'
+  end
+
+  it 'can deduct fare from my card' do
+    expect{ oystercard.deduct 25 }.to change{ oystercard.balance}.by -25
   end
 
 end
